d3cbd6fa4f69e125d58b0db5dedb3bbe51b25841
Twinkle Saggu <twinkle.saggu@viithiisys.com>
2016-07-20 11:35:31 +0530

    landing form

--- src/js/controllers/alert-ctrl.js
+++ src/js/controllers/alert-ctrl.js
@@ -7,21 +7,8 @@ angular
     .controller('AlertsCtrl', ['$scope', AlertsCtrl]);
 
 function AlertsCtrl($scope) {
-    $scope.alerts = [{
-        type: 'success',
-        msg: 'Thanks for visiting! Feel free to create pull requests to improve the dashboard!'
-    }, {
-        type: 'danger',
-        msg: 'Found a bug? Create an issue with as many details as you can.'
-    }];
-
-    $scope.addAlert = function() {
-        $scope.alerts.push({
-            msg: 'Another alert!'
-        });
-    };
-
-    $scope.closeAlert = function(index) {
-        $scope.alerts.splice(index, 1);
-    };
+    $scope.submitForm = function(){
+        console.log("============");
+    }
+    
 }
--- src/js/controllers/master-ctrl.js
+++ src/js/controllers/master-ctrl.js
@@ -3,37 +3,41 @@
  */
 
 angular.module('RDash')
-    .controller('MasterCtrl', ['$scope', '$cookieStore', MasterCtrl]);
+    .controller('MasterCtrl', ['$scope', MasterCtrl]);
 
-function MasterCtrl($scope, $cookieStore) {
-    /**
-     * Sidebar Toggle & Cookie Control
-     */
-    var mobileView = 992;
-
-    $scope.getWidth = function() {
-        return window.innerWidth;
-    };
-
-    $scope.$watch($scope.getWidth, function(newValue, oldValue) {
-        if (newValue >= mobileView) {
-            if (angular.isDefined($cookieStore.get('toggle'))) {
-                $scope.toggle = ! $cookieStore.get('toggle') ? false : true;
-            } else {
-                $scope.toggle = true;
-            }
-        } else {
-            $scope.toggle = false;
+function MasterCtrl($scope) {
+    $scope.formData = {};
+    $scope.registerData = {};
+    $scope.formfirst = true;
+    $scope.formsecond = false;
+    $scope.submitForm = function(){
+        $scope.formsecond = true;
+        $scope.formfirst = false;
+        console.log("$scope.formData.firstname",$scope.formData.firstname);
+        $scope.formData = {
+            firstname   : $scope.formData.firstname,
+            lastname    : $scope.formData.lastname,
+            number      : $scope.formData.number,
+            email       : $scope.formData.email,
+            password    : $scope.formData.password,
+            otp         : 555555  
         }
+        $scope.formDataSave = $scope.formData;
+        console.log("====formDataSave===",$scope.formDataSave);
+        console.log("======$scope.formData=====",JSON.stringify($scope.formData));
+    }
 
-    });
-
-    $scope.toggleSidebar = function() {
-        $scope.toggle = !$scope.toggle;
-        $cookieStore.put('toggle', $scope.toggle);
-    };
-
-    window.onresize = function() {
-        $scope.$apply();
-    };
+    $scope.register = function(){
+        $scope.registerData = {
+            data        : $scope.formDataSave,
+            college     : $scope.registerData.collegeDetails,
+            speciality  : $scope.registerData.speciality,
+            title       : $scope.registerData.title,
+            clinic      : $scope.registerData.clinic,
+            state       : $scope.registerData.state
+        }
+        $scope.registeredData = $scope.registerData;
+        console.log("=====registeredData======",$scope.registeredData);
+        console.log("=====$scope.registerData======",JSON.stringify($scope.registerData));
+    }
 }
