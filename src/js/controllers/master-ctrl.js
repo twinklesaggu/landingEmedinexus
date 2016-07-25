dfb01fdcab0436878d21b16fa6e7ab90b287e2fb
Twinkle Saggu <twinkle.saggu@viithiisys.com>
2016-07-25 13:03:12 +0530

    code added corasuel working

--- src/js/controllers/master-ctrl.js
+++ src/js/controllers/master-ctrl.js
@@ -11,52 +11,83 @@ function MasterCtrl($scope) {
     $scope.registerData = {};
     $scope.formfirst = true;
     $scope.formsecond = false;
+    $scope.backToform1 = function(){
+        $scope.formfirst = true;
+        $scope.formsecond = false;
+    }
     $scope.submitForm = function(){
-        $scope.formsecond = true;
-        $scope.formfirst = false;
-        console.log("$scope.formData.firstname",$scope.formData.firstname);
-        $scope.formData = {
-            firstname   : $scope.formData.firstname,
-            lastname    : $scope.formData.lastname,
-            number      : $scope.formData.number,
-            email       : $scope.formData.email,
-            college    : formData.collegeDetails,
-            otp         : 555555  
-        }
-        $scope.formDataSave = $scope.formData;
-        console.log("====formDataSave===",$scope.formDataSave);
-        console.log("======$scope.formData=====",JSON.stringify($scope.formData));
+      $scope.formsecond = true;
+      $scope.formfirst = false;
+      console.log("$scope.formData.firstname",$scope.formData.firstname);
+      $scope.formData = {
+          firstname   : $scope.formData.firstname,
+          lastname    : $scope.formData.lastname,
+          number      : $scope.formData.number,
+          email       : $scope.formData.email,
+          college     : $scope.formData.collegeDetails,
+          otp         : 555555  
+      }
+      $scope.formDataSave = $scope.formData;
+      console.log("====formDataSave===",$scope.formDataSave);
+      console.log("======$scope.formData=====",JSON.stringify($scope.formData));
     }
 
     $scope.register = function(){
-        $scope.registerData = {
-            gender       : $scope.registerData.gender,
-            data        : $scope.formDataSave,
-            college     : $scope.registerData.collegeDetails,
-            speciality  : $scope.registerData.speciality,
-            title       : $scope.registerData.title,
-            clinic      : $scope.registerData.clinic,
-            state       : $scope.registerData.state
-        }
-        $scope.registeredData = $scope.registerData;
-        console.log("=====registeredData======",$scope.registeredData);
-        console.log("=====$scope.registerData======",JSON.stringify($scope.registerData));
+      $scope.dummyTitle   = "others"; //dummy value title
+      $scope.dummyClinic  = "others"  //dummy value clinic
+      $scope.registerData = {
+          data        : $scope.formDataSave,
+          gender      : $scope.registerData.gender,
+          college     : $scope.registerData.speciality,
+          speciality  : $scope.registerData.degree,
+          title       : $scope.dummyTitle,    
+          clinic      : $scope.dummyClinic,  
+          state       : $scope.registerData.statemedicalcouncil,
+          mcinumber   : $scope.registerData.mci_number 
+      }
+      $scope.registeredData = $scope.registerData;
+      console.log("=====registeredData======",$scope.registeredData);
+      console.log("=====$scope.registerData======",JSON.stringify($scope.registerData));
     }
-    $scope.slides =[ 
-        {
-            image: 'img/media-one.jpg'
-        },
-        {
-            image: 'img/media-one.jpg'
-        },
-        {
-            image: 'img/media-one.jpg'
-        },
-        {
-            image: 'img/media-one.jpg'
-        },
-        {
-            image: 'img/media-one.jpg'
-        },
-    ]
+    $(document).ready(function() {
+ 
+        $("#owl-demo").owlCarousel({
+     
+          navigation : false, // Show next and prev buttons
+          autoPlay: true,   
+          slideSpeed : 300,
+          paginationSpeed : 400,
+     
+          items : 5, 
+          itemsDesktop : false,
+          itemsDesktopSmall : [979,3],
+          itemsTablet: [768,2],
+          itemsMobile : [479,1]
+     
+        });
+
+        $("#owl-two").owlCarousel({
+     
+          navigation : false, // Show next and prev buttons
+          autoPlay: true,   
+          slideSpeed : 500,
+          paginationSpeed : 400,
+          singleItem:true,
+          stopOnHover: true
+     
+          // items : 1, 
+          // itemsDesktop : false,
+          // itemsDesktopSmall : false,
+          // itemsTablet: false,
+          // itemsMobile : false
+     
+        });
+
+        // $(window).scroll(function() {
+        //    var winScroll = $(window).scrollTop();
+        //    var finalScrll = winScroll / 100;           
+        //    $("#toScroll").css('backgroundPosition': 0+'px' +finalScrll+'px');          
+        // });
+ 
+    });
 }
